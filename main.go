ec1b7ae7b4bcd50d197fa7402457bdb6fff50a10
sasa0xx <vole2019123@gmail.xom>
2026-01-05 19:52:07 +0000

    made a test server

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", HandleRoot)
+	http.ListenAndServe(":8080", mux)
+}
+
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello, World! :D"))
+}
