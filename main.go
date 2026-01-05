4a969a8c6e54eb832abed1c11eae3fce3add62ff
sasa0xx <vole2019123@gmail.xom>
2026-01-05 21:09:24 +0000

    Added post functionality

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"sync"
+)
+
+type User struct {
+	Name string `json:"name"`
+}
+
+var usersCache = make(map[int]User)
+var cacheMutex sync.RWMutex
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", HandleRoot)
+	mux.HandleFunc("POST /users", CreateUser)
 	http.ListenAndServe(":8080", mux)
 }
 
@@ -13,4 +25,22 @@ func HandleRoot(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello, World! :D"))
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if user.Name == "" {
+		http.Error(w, "User name is required", http.StatusBadRequest)
+		return
+	}
+
+	cacheMutex.Lock()
+	usersCache[len(usersCache)] = user
+	cacheMutex.Unlock()
+	w.WriteHeader(http.StatusNoContent)
 }
